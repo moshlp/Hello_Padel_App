47603fbdfcc9845a2fa9a3fb9c07fcf5f39f4169
damianperon <dperon@gmail.com>
2021-02-14 13:18:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hellopadelapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.moshlp.hellopadelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("https://hellopadelacademy.com/")
+    }
+}
